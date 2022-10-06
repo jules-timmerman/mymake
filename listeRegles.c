fd5117dd85ba18ee3c11bf45b29ac7cd9407bc60
guilhem repetto <guilhem.repetto@ens-rennes.fr>
2022-10-06 14:30:46 +0200

    Il n'y a plus d'erreur de compilation, WTF ?!

--- listeRegles.c
+++ listeRegles.c
@@ -1,11 +1,14 @@
 #include "listeRegles.h"
 #include <stdlib.h>
 
+// Une liste de règles est soit le pointeur NULL,
+// soit un couple composé d'une règle et d'un pointeur vers une liste de règles
 struct listeRegles{
 	regle_t* regle;
 	listeRegles_t* next; // NULL quand le dernier
 };
 
+// Ajoute une règle [r] à la liste de règles [list]
 listeRegles_t* addRegle(listeRegles_t* list, regle_t* r){
 	listeRegles_t* retList = malloc(sizeof(listeRegles_t));
 	retList->regle = r;
@@ -13,15 +16,17 @@ listeRegles_t* addRegle(listeRegles_t* list, regle_t* r){
 	return retList;
 }
 
-
+// Renvoie un pointeur vers la règle de nom [nom], et NULL si une telle règle n'existe pas
 regle_t* rechercheRegle(listeRegles_t* list, char nom){
 	if (list == NULL){
 		return NULL;
 	}
-	else if (list->regle->nom == nom) {
+	else {
+		if (*(list->regle->nom) == nom) {
 		return list->regle;
 	}
 	else {
 		rechercheRegle(list->next, nom);
+	}
 	}
 }
--- listeRegles.h
+++ listeRegles.h
@@ -5,7 +5,7 @@
 
 typedef struct listeRegles listeRegles_t;
 
-listeRegles_t* addRegle(listeRegles_t* list, regle_t* r); // Ajoute en tête et renvoit une liste (cf CAML ::)
+listeRegles_t* addRegle(listeRegles_t* list, regle_t* r); // Ajoute en tête et renvoie une liste (cf CAML ::)
 
 regle_t* rechercheRegle(listeRegles_t* list, char nom);
 
