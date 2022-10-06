2b5c40a9d7bd7ca7ff6272ae874dc1870b752404
guilhem repetto <guilhem.repetto@ens-rennes.fr>
2022-10-06 22:26:27 +0200

    Nimportenawak

--- main.c
+++ main.c
@@ -4,20 +4,21 @@
 #include "regle.h"
 #include "listeRegles.h"
 
+
+/* Transforme une regle de type main: main.o regle.o listeRegles.o */
 listeRegles_t readMakefile(FILE* makefile){
-
-    if (makefile == NULL){
-        return createListeRegle();
+    char ligne[512];
+    listeRegles_t* liste = createListeRegle(); 
+    while (getline(makefile, "%s", &ligne)){
+        if (ligne[0] != '	') {
+            char** decoupe;
+            regle_t* nouvelleRegle;
+            decoupe = strtok (ligne, ' :\t');
+            nouvelleRegle = createRegle(decoupe[0], decoupe, decoupe);
+            liste = addRegle(liste, nouvelleRegle);
+           }
     }
-    else{
-        char ligne;
-        while (getline(makefile, "%c", &ligne) != EOF)
-        {
-            
-        }
-        
-
-    }
+    return *liste;
 }
 
 
