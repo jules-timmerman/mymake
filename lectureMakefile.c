afad9749415ebed5e060daa217e7cf54a6cc82a8
snoopinou30 <jules.snoopi@live.fr>
2022-10-08 18:37:15 +0200

    Comme avant mais j'avais oublié

--- lectureMakefile.c
+++ lectureMakefile.c
@@ -4,7 +4,6 @@
 #include <string.h>
 #include <stdbool.h>
 
-#include "listeRegles.h"
 #include "listeCommandes.h"
 
 
--- lectureMakefile.h
+++ lectureMakefile.h
@@ -1,8 +1,10 @@
 #ifndef LECTUREMAKEFILE_H
 #define LECTUREMAKEFILE_H
 
-
+#include "listeRegles.h"
 #include <stdio.h>
+
+
 
 listeRegles_t* makefile2list(FILE* makefile);
 
